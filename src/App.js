d0f1f0ca021a0a9eacc7824e7c5ff1cb2a52fdd3
Admin <you@example.com>
2022-02-16 20:57:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import photox from './photox.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+ 
+ 
+ state= {fullName:'houcine bouattour', bio:'developer',img: photox, profession:'developer full Stack',show: false, counter:0 }
+ 
+ 
+
+         
+showContent=()=>{
+
+  if(this.state.show)
+  {
+    this.setState({show:false})
+   
+  }
+  else  {
+    setInterval(()=>{
+        this.setState({counter:this.state.counter+1}) }, 1000  );      
+        this.setState({show:true})
+ this.setState({counter:0} )
+
+       }
+   
+ }
+
+ render() {
+   return  <div  style={{border:"solid 1px black",maxWidth:"100vw", textAlign: "center" }}>
+     
+     {this.state.show ? <div >
+   <p> <img src={this.state.img}></img></p>
+   <h1>{this.state.fullName}</h1>
+   <p>{this.state.bio}</p>
+   <p>{this.state.profession }</p>
+   <h1>{this.state.counter}</h1>
+   </div> :''}
+   <button 
+       style={{backgroundColor:"red" ,border:"solid 1px black",maxWidth:"100vw", textAlign: "center"}}  
+     
+        onClick={this.showContent}>welcome</button>
+   
+         
+   
+  
+ </div>
+ }
 }
 
-export default App;
